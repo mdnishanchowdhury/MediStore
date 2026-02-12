59da447e5cc31ed3c443ac62cbe7c315c52e9330
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2026-02-12 21:23:26 +0600

    add user admin view, delete, and update

--- src/Modules/users/user.routes.ts
+++ src/Modules/users/user.routes.ts
@@ -6,8 +6,8 @@ const router = express.Router();
 
 
 router.get("/users", userControllers.getUser);
-router.get("/users/:id",auth(UserRole.CUSTOMER, UserRole.ADMIN, UserRole.SELLER), userControllers.getSingleUser);
-router.put("/users/:id",auth(UserRole.CUSTOMER, UserRole.ADMIN, UserRole.SELLER), userControllers.putSingleUser);
-router.delete("/users/:id", auth(UserRole.CUSTOMER, UserRole.ADMIN, UserRole.SELLER), userControllers.deleteSingleUser);
+router.get("/users/:id", auth(UserRole.CUSTOMER, UserRole.ADMIN, UserRole.SELLER), userControllers.getSingleUser);
+router.put("/users/:id", auth(UserRole.ADMIN, UserRole.SELLER), userControllers.putSingleUser);
+router.delete("/users/:id", auth(UserRole.ADMIN, UserRole.SELLER), userControllers.deleteSingleUser);
 
 export const userRoutes = router;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // true if port 465
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
